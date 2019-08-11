2df70a4eab30b1ec3c8e39068a951ebe9aed7b51
Christopher Escalon <escalonchristopher@gmail.com>
2019-08-11 00:40:58 -0600

    working on connection management

---
+++ SybaseManager/ConnectionForm.cs
@@ -0,0 +1,12 @@
+using System.Windows.Forms;
+
+namespace SybaseManager
+{
+    public partial class ConnectionForm : Form
+    {
+        public ConnectionForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
